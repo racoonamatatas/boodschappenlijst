88711d3e86320af008e49bfd150021a9c3d06246
racoonamatatas <emacanghaill@gmail.com>
2026-01-20 14:43:44 +0100

    setup router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
-<script setup>
-import {ref} from 'vue';
-import GroceriesTable from './components/GroceriesTable.vue';
-
-const products = ref([
-    {item: 'Rice', price: 1.0, quantity: 0},
-    {item: 'Broccoli', price: 0.99, quantity: 0},
-    {item: 'Biscuits', price: 1.2, quantity: 0},
-    {item: 'Nuts', price: 2.99, quantity: 0},
-]);
-</script>
-
 <template>
     <div>
-        <GroceriesTable :products="products" />
+        <nav>
+            <RouterLink to="/overview">Overview</RouterLink>
+            <RouterLink to="/create">Create</RouterLink>
+        </nav>
+    </div>
+    <div>
+        <main>
+            <RouterView />
+        </main>
     </div>
 </template>
 
