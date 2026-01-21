d894be5ed9955f67c29f53411b27c919e9fd1da4
racoonamatatas <emacanghaill@gmail.com>
2026-01-21 11:34:13 +0100

    Initial commit

---
+++ src/domains/groceries/components/GroceryForm.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {ref} from 'vue';
+
+const props = defineProps(['grocery']);
+const emit = defineEmits(['submit']);
+
+const name = ref(props.grocery.name);
+const price = ref(props.grocery.price);
+const quantity = ref(props.grocery.amount);
+
+const createGrocery = () => ({name: name.value, price: price.value, quantity: quantity.value});
+
+const emitEvent = () => {
+    emit('submit', createGrocery);
+};
+</script>
+
+<template>
+    <input v-model.trim="name" type="text" />
+    <input v-model.number="price" />
+    <input v-model.number="amount" />
+    <button @click="emitEvent">Submit</button>
+</template>
