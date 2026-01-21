68d976e85f541fa1feee207e5e6f8bbc128ef5ad
racoonamatatas <emacanghaill@gmail.com>
2026-01-21 11:54:15 +0100

    feat: grocery template to pass to GroceryForm.vue

--- src/domains/groceries/pages/Create.vue
+++ src/domains/groceries/pages/Create.vue
@@ -1,1 +1,11 @@
-<template></template>
+<script setup>
+import {ref} from 'vue';
+import GroceryForm from '../components/GroceryForm.vue';
+import {addGrocery} from '../store';
+
+const grocery = ref({name: '', price: 0, amount: 0});
+</script>
+
+<template>
+    <GroceryForm :grocery="grocery" @submit="addGrocery" />
+</template>
