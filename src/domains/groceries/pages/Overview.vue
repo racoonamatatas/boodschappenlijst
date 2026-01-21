8e6df55dc8e0c76a28ba74174b8ff27ea27c5f68
racoonamatatas <emacanghaill@gmail.com>
2026-01-21 10:34:16 +0100

    refactor: pass array to child via store getter

--- src/domains/groceries/pages/Overview.vue
+++ src/domains/groceries/pages/Overview.vue
@@ -1,13 +1,9 @@
 <script setup>
 import {ref} from 'vue';
 import GroceriesTable from '../components/GroceriesTable.vue';
+import {getAllGroceries} from '../store';
 
-const products = ref([
-    {item: 'Rice', price: 1.0, quantity: 0},
-    {item: 'Broccoli', price: 0.99, quantity: 0},
-    {item: 'Biscuits', price: 1.2, quantity: 0},
-    {item: 'Nuts', price: 2.99, quantity: 0},
-]);
+const products = getAllGroceries(); // pass the getter directly?
 </script>
 
 <template>
